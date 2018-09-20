86d48a03954aed76466f7c7a9d63a61584a88db2
Sebastian Edholm <sebastian.edholm@gmail.com>
2018-09-20 15:58:21 +0200

    Added age property and setter

--- programmering_systemering/v39/lek2/class3.py
+++ programmering_systemering/v39/lek2/class3.py
@@ -1,6 +1,12 @@
+# Fördelen med properties är att du har ett sett hämta och sätta attribut oavsett om variablen
+# är private eller public.
+# Du kan köra kontrollers i ditt attribut för att kolla om värdet är ok.
+
 class Person:
-    def __init__(self, name):
+    """ This is my person class. Can I do this better?"""
+    def __init__(self, name, age):
         self.__name = name
+        self.__age = age
 
     @property
     def formats(self):
@@ -11,13 +17,27 @@ class Person:
     def name(self):
         return self.__name
 
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, age):
+        if age > 120:
+            self.__age = 80
+            print("That's too old, age is set to 80")
+        else:
+            self.__age
+
     @name.setter
     def name(self, name):
         self.__name = name
 
 
 
-p1 = Person('Sebastian')
+p1 = Person('Sebastian', 32)
 print(p1.formats)
 p1.name =  'John'
 print(p1.formats)
+p1.age = 155
+print(p1.age)
